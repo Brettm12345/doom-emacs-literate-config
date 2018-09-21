0c7a37498ebe6ae8df88458e7c29dfa3463bbb8a
Brett Mandler <brettmandler@gmail.com>
2018-09-21 02:28:58 +0000

    Added yarn support

--- packages.el
+++ packages.el
@@ -5,6 +5,9 @@
 (package! evil-lion)
 (package! evil-replace-with-register)
 
+;; lang/javascript
+(package! yarn :recipe (:fetcher github :repo "jmfirth/yarn.el"))
+
 ;; lang/sh
 (package! flycheck-checkbashisms)
 
