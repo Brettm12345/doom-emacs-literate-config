5e489e630f8946be33f91db7d5fbb8ea8586774f
Brett Mandler <brettmandler@gmail.com>
2018-11-09 05:47:09 +0000

    Added apache mode

--- packages.el
+++ packages.el
@@ -4,11 +4,14 @@
 ;; app/transmission
 (package! transmission)
 
-;; feature/evil
-(package! evil-replace-with-register)
-
 ;; lang/javascript
 (package! yarn :recipe (:fetcher github :repo "jmfirth/yarn.el"))
 
+;; lang/apache
+(package! apache-mode)
+
 ;; lang/sh
 (package! flycheck-checkbashisms)
+
+;; ui/doom
+(package! doom-themes :recipe (:fetcher github :repo "brettm12345/emacs-doom-themes" :files ("*.el" "themes/*.el")))
